17e45a570c36b5aaffa7e29214b9c1c1e7c38193
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-05-29 12:07:24 +0500

    Добавление класса Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
 
 
