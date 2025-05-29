4af019bd3cc0857dae22cf2e86f49d7d8ddbb1e9
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-05-29 12:17:25 +0500

    Тема: Наследование

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,9 +6,34 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee employee = new Employee {Name="Tom" };
+
+            employee.Company = "Microsoft";
+
+            employee.Print();
+
+            employee.PrintCompany();
         }
     }
 }
 
 
+class Person
+{
+    public string Name { get; set; }
+
+    public void Print()
+    {
+        Console.WriteLine(Name);
+    }
+}
+
+class Employee : Person
+{
+    public string? Company { get; set; }
+
+    public void PrintCompany()
+    {
+        Console.WriteLine($"{Name} works at {Company}");
+    }
+}
