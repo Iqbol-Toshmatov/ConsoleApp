2e2be6bdfce0bc0c641fe8883bc3ff0d303d0810
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-05-30 14:38:35 +0500

    Тема : Абстрактные классы и члены классов

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,36 +6,116 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Person person = new Person { Name = "Alice" };
 
-            Employee employee = new Employee {Name="Tom" };
+            Animal dog=new Dog("Buddy");
+            dog.Sound();
 
-            employee.Company = "Microsoft";
+            Transport plane = new Plane();
+            plane.Move();
 
-            person.Print();
+            Shape rectangle=new Rectangle(5, 10);
 
-            employee.Print();
+            var perimeter = rectangle.GetPerimeter();
+            Console.WriteLine("Perimeter of rectangle : " + perimeter);
+
+            var area = rectangle.GetArea();
+            Console.WriteLine("Area of rectangle : " + area);
+
         }
     }
-}
 
-
-class Person
-{
-    public string Name { get; set; }
-
-    public virtual void Print()
+    // Абстрактный класс Animal представляет животное с именем и методом Sound
+    public abstract class Animal
     {
-        Console.WriteLine(Name);
+
+        // Свойство для хранения имени животного
+        public string Name { get; set; }
+
+
+        // Конструктор, инициализирующий имя животного
+        protected Animal(string name)
+        {
+            Name = name;
+        }
+
+
+        // Метод для вывода звука животного (может быть переопределён в наследниках)
+        public void Sound()
+        {
+            Console.WriteLine($"{Name}'s sound");
+        }
     }
-}
 
-class Employee : Person
-{
-    public string? Company { get; set; }
-
-    public override void Print()
+    // Класс Dog наследует Animal и реализует конструктор
+    public class Dog : Animal
     {
-        Console.WriteLine($"{Name} works at {Company}");
+
+        // Конструктор принимает имя и передает его базовому классу Animal
+        public Dog(string name) : base(name)
+        {
+        }
+
     }
+
+    // Абстрактный класс Transport определяет общий интерфейс для транспорта
+    public abstract class Transport
+    {
+
+        // Абстрактный метод Move должен быть реализован в наследниках
+        public abstract void Move();
+    }
+
+    // Класс Plane наследует Transport и реализует метод Move
+    public class Plane : Transport
+    {
+
+        // Реализация метода Move для самолета
+        public override void Move()
+        {
+            Console.WriteLine("Plane is flying");
+        }
+    }
+
+
+    // Абстрактный класс Shape определяет интерфейс для геометрических фигур
+    public abstract class Shape
+    {
+        // Абстрактный метод для вычисления периметра фигуры
+        public abstract double GetPerimeter();
+
+        // Абстрактный метод для вычисления площади фигуры
+        public abstract double GetArea();
+    }
+
+    // Класс Rectangle реализует фигуру прямоугольник
+    public class Rectangle : Shape
+    {
+        // Свойство высоты прямоугольника
+        public double Height { get; set; }
+        // Свойство ширины прямоугольника
+        public double Width { get; set; }
+
+        // Конструктор принимает высоту и ширину прямоугольника
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        // Переопределение метода для вычисления периметра прямоугольника
+        public override double GetPerimeter()
+        {
+            return 2 * (Height + Width);
+        }
+
+        // Переопределение метода для вычисления площади прямоугольника
+        public override double GetArea()
+        {
+            return Height * Width;
+        }
+    }
+
+
 }
+
+
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e45a570c36b5aaffa7e29214b9c1c1e7c38193")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0f681c4b38634bb0192ea1e08e0ecbba76d506f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
