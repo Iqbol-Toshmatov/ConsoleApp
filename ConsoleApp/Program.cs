87bfbf3b8877a099ca8f5f60d102fae71249e817
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-06-02 15:46:03 +0500

    Тема : Использование делегатов

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace ConsoleApp
 {
@@ -6,25 +7,52 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            try
-            {
-                int x = 5;
-                int y = x / 1;
-                Console.WriteLine(y);
+            Account account = new Account(200);
+            account.Register(PrintMessage);
+            account.Add(200);
+            account.Take(100);
+            account.Take(400);
 
-
-            }
-            catch
-            {
-                Console.WriteLine("Exception");
-            }
-            finally
-            {
-                Console.WriteLine("End of program");
-            }
+            void PrintMessage(string message)=>Console.WriteLine(message);
         }
     }
 
+    public delegate void Message(string message);
+
+    public class Account
+    {
+        int sum;
+        Message? taken;
+
+        public void Register(Message del)
+        {
+            taken = del;
+        }
+
+        public Account(int sum)
+        {
+            this.sum = sum;
+        }
+
+
+        public void Add(int sum)
+        {
+            this.sum += sum;
+            taken?.Invoke($"\nПриход {sum} у.е. \n\tБаланс: {this.sum} у.е.");
+
+        }
+
+        public void Take(int sum)
+        {
+            if (sum <=this.sum)
+            {
+                this.sum -= sum;
+                taken?.Invoke($"\nСо счета списано {sum} у.е. \n\tБаланс: {this.sum} у.е.");
+            }
+            else
+                taken?.Invoke($"\nНедостаточно средств. Вы пытались списать {sum} \nБаланс: {this.sum} у.е.");
+        }
+    }
 }
 
 
