56171282fbe505b887b14ae35f3fb31ea0c5ba86
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-06-03 15:47:31 +0500

    Тема : Интерфейсы

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,52 +7,25 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Account account = new Account(200);
-            account.Register(PrintMessage);
-            account.Add(200);
-            account.Take(100);
-            account.Take(400);
-
-            void PrintMessage(string message)=>Console.WriteLine(message);
+            Car car = new Car();
+            car.Move();
         }
     }
 
-    public delegate void Message(string message);
-
-    public class Account
+    interface IMovable
     {
-        int sum;
-        Message? taken;
-
-        public void Register(Message del)
+        public void Move()
         {
-            taken = del;
+            Console.WriteLine("Walking");
         }
+    }
 
-        public Account(int sum)
+    class Car : IMovable
+    {
+        public void Move()
         {
-            this.sum = sum;
-        }
-
-
-        public void Add(int sum)
-        {
-            this.sum += sum;
-            taken?.Invoke($"\nПриход {sum} у.е. \n\tБаланс: {this.sum} у.е.");
-
-        }
-
-        public void Take(int sum)
-        {
-            if (sum <=this.sum)
-            {
-                this.sum -= sum;
-                taken?.Invoke($"\nСо счета списано {sum} у.е. \n\tБаланс: {this.sum} у.е.");
-            }
-            else
-                taken?.Invoke($"\nНедостаточно средств. Вы пытались списать {sum} \nБаланс: {this.sum} у.е.");
+            Console.WriteLine("Car is moving");
         }
     }
 }
-
 
