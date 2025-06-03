ad1acd1fdd60500e8a7bbf083d4b735f304f421a
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-06-03 16:17:36 +0500

    Тема : Коллекции

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,28 +7,60 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Message message = new Message { Text = "Hello, World!" };
-            message.Print();
+            List<string> names = new List<string> {"Tom", "Sam", "Bob" };
+
+            List<Person> personNames = new List<Person>()
+            {
+                new Person("Alice"),
+                new Person("Tommy"),
+                new Person("Jimmy")
+            };
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.Add("John");
+            names.AddRange(new[] {"Bib","Tim" });
+            names.Insert(0,"Me");
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.Remove("Tom");
+            names.RemoveAt(1);
+            names.RemoveAll(name => name.Length < 3);
+            names.Clear();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            var excistsLength2 = names.Exists(name => name.Length == 2);
+            Console.WriteLine(excistsLength2);
+
+            names.Reverse();
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
         }
     }
 
-    interface IMessage
-    { 
-        string Text { get; set; }
-    }
-
-    public interface IPrintable
+    class Person
     {
-        void Print();
-    }
+        public string Name { get; set; }
 
-    class Message : IMessage, IPrintable
-    {
-        public string? Text { get; set; }
-
-        public void Print()
+        public Person(string name)
         {
-            Console.WriteLine(Text);
+            Name = name;
         }
     }
 
