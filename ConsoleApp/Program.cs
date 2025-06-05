7b3eed4a50e54e789ebfb226eb8e72172461c465
Iqbol Toshmatov <iqbolToshmatov1105@gmail.com>
2025-06-05 09:31:50 +0500

    Тема : Queue

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,62 +7,38 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string> {"Tom", "Sam", "Bob" };
+            Queue<Person> patiens = new Queue<Person>() ;
+            patiens.Enqueue(new Person("Tom"));
+            patiens.Enqueue(new Person("Sam"));
+            patiens.Enqueue(new Person("Bob"));
+            patiens.Enqueue(new Person("Tim"));
 
-            List<Person> personNames = new List<Person>()
-            {
-                new Person("Alice"),
-                new Person("Tommy"),
-                new Person("Jimmy")
-            };
-
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
-
-            names.Add("John");
-            names.AddRange(new[] {"Bib","Tim" });
-            names.Insert(0,"Me");
-
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
-
-            names.Remove("Tom");
-            names.RemoveAt(1);
-            names.RemoveAll(name => name.Length < 3);
-            names.Clear();
-
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
-
-            var excistsLength2 = names.Exists(name => name.Length == 2);
-            Console.WriteLine(excistsLength2);
-
-            names.Reverse();
-
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
-
-
+            Doctor doctor = new Doctor();
+            doctor.TakePatiens(patiens);
         }
     }
 
     class Person
     {
         public string Name { get; set; }
-
         public Person(string name)
         {
-            Name = name;
+            Name=name;  
         }
     }
 
+    class Doctor
+    {
+        public void TakePatiens(Queue<Person> patiens)
+        {
+            while (patiens.Count>0)
+            {
+                var patien = patiens.Dequeue();
+                Console.WriteLine($"Осмотр пациента {patien.Name}");
+            }
+
+            Console.WriteLine("Queue is empty!");
+        }
+    }
 }
 
